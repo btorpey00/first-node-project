bea38eb812047b60d96bc9f2bc8a7382d8391811
btorpey00 <120498601+btorpey00@users.noreply.github.com>
2023-05-01 18:29:31 -0400

    refactor using express

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-let http = require('http');
-let url = require('url');
-let fs = require('fs');
+// let http = require('http');
+// let url = require('url');
+// let fs = require('fs');
 
-http.createServer(function (req, res) {
+// http.createServer(function (req, res) {
     
-    let filename = "./";
+//     let filename = "./";
     
-    switch(req.url) {
-        case '/':
-            filename += 'index.html';
-            res.statusCode = 200;
-            break;
-        case '/about':
-            filename += 'about.html';
-            res.statusCode = 200;
-            break;
-        case '/contact-me':
-            filename += 'contact-me.html';
-            res.statusCode = 200;
-            break;
-        default:
-            filename += '404.html'
-            res.statusCode = 404;
-    }
+//     switch(req.url) {
+//         case '/':
+//             filename += 'index.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/about':
+//             filename += 'about.html';
+//             res.statusCode = 200;
+//             break;
+//         case '/contact':
+//             filename += 'contact-me.html';
+//             res.statusCode = 200;
+//             break;
+//         default:
+//             filename += '404.html'
+//             res.statusCode = 404;
+//     }
 
 
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+//     fs.readFile(filename, function(err, data) {
+//         if (err) {
+//             res.writeHead(404, {'Content-Type': 'text/html'});
+//             return res.end("404 Not Found");
+//         }
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', (req, res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/contact', (req, res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname + '/contact-me.html');
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/404.html')
+})
+
+app.listen(8080);
